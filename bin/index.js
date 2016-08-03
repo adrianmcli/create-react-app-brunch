de9008d750056030387974df036ac53eab9d7de3
Adrian Li <li.adrianmc@gmail.com>
2016-08-04 06:02:55 +0800

    add basic script to create a brunch project

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,24 @@
 #! /usr/bin/env node
 var shell = require("shelljs");
+var chalk = require("chalk");
 
-shell.exec("echo Hello World!");
-shell.exec("echo shell.exec works");
+var error = chalk.bold.red;
+var success = chalk.bold.green;
+
+if (!process.argv[2]) {
+  console.log(error('You need to supply a name for your app!'));
+  console.log();
+  console.log('Try:');
+  console.log('        create-react-app-brunch <project-name>');
+} else {
+  var appName = process.argv[2];
+  console.log('Your app is now being created...');
+  
+  console.log('Installing Brunch...');
+  shell.exec('npm install --save brunch');
+  
+  console.log('Creating React app with Brunch Skeleton...');
+  shell.exec('./node_modules/brunch/bin/brunch new ' + appName + ' -s react');
+
+  console.log(success('Success! Your app is done.'))
+}
