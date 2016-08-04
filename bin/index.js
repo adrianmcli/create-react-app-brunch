516a6dfd582232538be4400d9bc3346d7c3eac2a
Adrian Li <li.adrianmc@gmail.com>
2016-08-04 22:10:36 +0800

    improve prompts and instructions

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,31 @@
 #! /usr/bin/env node
-var shell = require("shelljs");
+var shell = require('shelljs');
 var chalk = require("chalk");
 
 var error = chalk.bold.red;
 var success = chalk.bold.green;
 
+console.log();
+
 if (!process.argv[2]) {
-  console.log(error('You need to supply a name for your app!'));
-  console.log();
-  console.log('Try:');
-  console.log('        create-react-app-brunch <project-name>');
+  console.log(error('Error: ') + 'You need to supply a name for your app!\n');
+  console.log('Try:  create-react-app-brunch <project-name>\n');
 } else {
   var appName = process.argv[2];
+  var appDir = shell.pwd().stdout + '/' + appName;
   
-  console.log('Creating React app with Brunch Skeleton...');
+  console.log('Creating a new React app in ' + appDir + '.\n');
+
   shell.exec('PATH=$(npm bin):$PATH brunch new ' + appName + ' -s adrianmc/cra-with-brunch');
 
-  console.log(success('Success! Your app is done.'))
+  console.log(success('Success! ') + 'Created ' + appName + ' at ' + appDir + '.');
+  console.log('Inside that directory, you can run the following commands:\n');
+
+  console.log('* npm start: Starts the development server.');
+  console.log('* npm run build: Bundles the app into static files for production.\n');
+
+  console.log('We suggest that you begin by typing:\n')
+  console.log('  cd ' + appName);
+  console.log('  npm start\n');
+  console.log('Happy hacking!')
 }
